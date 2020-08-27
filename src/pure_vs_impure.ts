6930690e18d0704e875d7d07b079e3b02bea41e0
dlrandy <1208484996@qq.com>
2020-08-28 06:49:09 +0800

    feat: init

---
+++ src/pure_vs_impure.ts
@@ -0,0 +1,31 @@
+module impure {
+  function isIndexPage() {
+    return window.location.pathname === '/';
+  }
+}
+
+module pure {
+  function isIndexPage(pathname:string) {
+    return pathname === '/';
+  }
+
+  function shouldReturnTrueWhenPathIsIndex() {
+    let expected = true;
+    let result = isIndexPage('/');
+    if (expected !== result) {
+      throw new Error(`Expected ${expected} to equal ${result}`);
+    }
+  }
+  function shouldReturnFalseWhenPathIsNotIndex() {
+    let expected = false;
+    let result = isIndexPage('/someotherpage');
+    if (expected !== result) {
+      throw new Error(`Expected ${expected} to equal ${result}`);
+    }
+  }
+}
+
+
+
+
+
