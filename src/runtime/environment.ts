280fb32619e99e790db9975ae500c8d991490d5d
dlrandy <1208484996@qq.com>
2020-09-06 12:14:43 +0800

    feat: runtime doen

---
+++ src/runtime/environment.ts
@@ -0,0 +1,11 @@
+namespace environment{
+  if (Promise && typeof Promise.all === 'function') {
+    
+  }
+
+  if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1 &&
+    navigator.vendor.toLowerCase().indexOf('google') > -1
+  ) {
+    
+  }
+}
